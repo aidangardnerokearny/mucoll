45465abde13c9d452011c9ea6fbc3372a6e65d9e
Aidan Gardner-O'Kearny <aidangardnerokearny@ap23.uc.osg-htc.org>
2026-05-26 10:57:14 -0500

    Start of week commit

--- analysisCode/efficiencyProblem/efficiencyChecker.py
+++ analysisCode/efficiencyProblem/efficiencyChecker.py
@@ -1,13 +1,13 @@
 import ROOT
+import math
 import os
 import glob
-import pyLCIO
-import math
 
-plot_dir = "/home/aidangardnerokearny/mucoll/analysisCode/testing/testingPlots"
+plot_dir = "/home/aidangardnerokearny/mucoll/analysisCode/efficiencyProblem/efficiencyPlots"
 os.makedirs(plot_dir, exist_ok=True)
 
-histogram = ROOT.TH1D("test", "", 100, 0, 100)
+efficiencyHisto = ROOT.TH1D("efficiency", "", 100, 0, 100)
+
 
 def getTLV(obj):
     obj_p = obj.getMomentum()
@@ -15,6 +15,7 @@ def getTLV(obj):
     obj_tlv = ROOT.TLorentzVector()
     obj_tlv.SetPxPyPzE(obj_p[0], obj_p[1], obj_p[2], obj_e)
     return obj_tlv
+
 
 reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
 reader.open("/home/aidangardnerokearny/mucoll/reco/electronGun_pT_0_50/electronGun_pT_0_50_reco_0.slcio")
@@ -29,21 +30,4 @@ for event in reader:
         if mcp.getGeneratorStatus() != 1 or abs(mcp.getPDG()) != 11: continue
         tlv = getTLV(mcp)
         print(tlv.E())
-        histogram.Fill(tlv.E()) 
-
-for i in range(10):
-    x = 0 
-
-c1 = ROOT.TCanvas("c")
-c1.SetTickx()
-c1.SetTicky()
-histogram.Draw("h")
-histogram.SetStats(0)
-histogram.Sumw2(ROOT.kFALSE)
-histogram.GetYaxis().SetTitle("entries")
-histogram.GetXaxis().SetTitle("energy")
-histogram.SetTitle("test")
-tl = ROOT.TLatex()
-tl.SetTextSize(0.02)
-
-c1.Print(f"{plot_dir}/testHistogram.pdf")
+ 
