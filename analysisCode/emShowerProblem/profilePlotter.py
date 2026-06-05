0dee7eff0bf972f30b7f418c955fdd98df27c6ac
Aidan Gardner-O'Kearny <aidangardnerokearny@ap23.uc.osg-htc.org>
2026-06-05 14:42:22 -0500

    New method of calculating plot maximum

--- analysisCode/emShowerProblem/profilePlotter.py
+++ analysisCode/emShowerProblem/profilePlotter.py
@@ -135,7 +135,9 @@ for idx, cl in enumerate(clusters):
     scale_meas = g_s_meas.Integral()
     scale_exp = g_s_exp.Integral()
 
-    y_max_s = max(g_s_meas.GetMaximum(), g_s_exp.GetMaximum()) * 1.15 # if all_y else 1.0
+    y_max_s = max(ROOT.TMath.MaxElement(g_s_meas.GetN(), g_s_meas.GetY()),
+                  ROOT.TMath.MaxElement(g_s_exp.GetN(), g_s_exp.GetY())) * 1.15 # if all_y else 1.0
+    print(y_max_s, g_s_meas.GetMaximum(), g_s_exp.GetMaximum())
 
     frame_s = c_single.DrawFrame(0, 0, x_max_s, y_max_s)
     frame_s.GetXaxis().SetTitle("Depth (radiation lengths X_{0})")
