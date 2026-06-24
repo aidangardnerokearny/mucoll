6822c87576b066128807674e80dff268fc004123
Aidan Gardner-O'Kearny <aidangardnerokearny@ap23.uc.osg-htc.org>
2026-06-24 13:23:07 -0500

    More bugfixing

--- jobs/run_sim.sh
+++ jobs/run_sim.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 set -eo pipefail
 
-
-# args
 JOB_NUM=$1
 INPUT_FILE=$2
 OUTPUT_FILE=$3
 COMPACT_FILE=$4
 
-echo "Using compact file: ${COMPACT_FILE}"
-echo "Job ${Job_NUM}"
-echo "Input: ${INPUT_FILE}"
-echo "Outpit: ${OUTPUT_FILE}"
+NEVENTS=1000
+SKIP=$(( JOB_NUM * NEVENTS ))
+
+echo "=== job ${JOB_NUM} on $(hostname) at $(date) ==="
+echo "compact: ${COMPACT_FILE}   input: ${INPUT_FILE}   skip: ${SKIP}"
+
+set +e
+source /opt/setup_mucoll.sh
+set -e
+
 which ddsim
 
-NEVENTS=1000
-SKIP=$((JOB_NUM * NEVENTS))
-
 ddsim \
-	--inputFile ${INPUT_FILE} \
-	--steeringFile sim_steer_GEN_CONDOR.py \
-	--outputFile ${OUTPUT_FILE} \
-	--numberOfEvents ${NEVENTS} \
-	--compactFile ${COMPACT_FILE} \
-	--skipNEvents ${SKIP} \
-	--random.seed $((JOB_NUM + 1)) \\
+    --inputFile      "${INPUT_FILE}" \
+    --steeringFile   sim_steer_GEN_CONDOR.py \
+    --compactFile    "${COMPACT_FILE}" \
+    --skipNEvents    "${SKIP}" \
+    --numberOfEvents "${NEVENTS}" \
+    --random.seed    $(( JOB_NUM + 1 )) \
+    --outputFile     "${OUTPUT_FILE}"
 
-
-echo "=== Done; wrote ${OUTPUT_FILE} === "
+echo "=== done; wrote ${OUTPUT_FILE} ==="
 ls -lh "${OUTPUT_FILE}"
