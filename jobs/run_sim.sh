31633cba8c74dfeddefc8f8ba1e29fa809707fb5
Aidan Gardner-O'Kearny <aidangardnerokearny@ap23.uc.osg-htc.org>
2026-06-24 09:39:42 -0500

    Updating jobs

--- jobs/run_sim.sh
+++ jobs/run_sim.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
+set -eo pipefail
+
 
 # args
 JOB_NUM=$1
 INPUT_FILE=$2
 OUTPUT_FILE=$3
+COMPACT_FILE=$4
 
-source ~/mucoll/.enterApptainer.sh
 
 source ~/mucoll/.setup_shower_profile.sh
 
-COMPACT_FILE=$4
 echo "Using compact file: ${COMPACT_FILE}"
-
-sed 's|SIM.compactFile = *|SIM.compactFile = \"${COMPACT_FILE}\"|' ~/mucoll/SteeringMacros/k4Reco/sim_steer_condor.py
 echo "Job ${Job_NUM}"
 echo "Input: ${INPUT_FILE}"
 echo "Outpit: ${OUTPUT_FILE}"
+which ddsim
 
+NEVENTS = 1000
+SKIP=$((JOB_NUM * NEVENTS))
 
 ddsim \
 	--inputFile ${INPUT_FILE} \
 	--steeringFile sim_steer_condor.py \
 	--outputFile ${OUTPUT_FILE} \
-	--numberOfEvents 1000 \
-	--compactFile ${COMPACT_FILE}
+	--numberOfEvents ${NEVENTS} \
+	--compactFile ${COMPACT_FILE} \
+	--skipNEvents ${SKIP} \
+	--random.seed $((JOB_NUM + 1)) \\
